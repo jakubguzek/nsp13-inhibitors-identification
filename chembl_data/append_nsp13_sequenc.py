0cde00dd34568d358eadb913c9b4327dc880d4c0
jakubguzek <jakub.j.guzek@gmail.com>
2024-01-20 13:27:27 +0100

    add: check whether nsp13 were already appended

--- chembl_data/append_nsp13_sequenc.py
+++ chembl_data/append_nsp13_sequenc.py
@@ -24,13 +24,30 @@ def get_nsp13_chembl_assay_ids(
 
 def main() -> int:
     assays_metadata_path = pathlib.Path("./activities_targets/assays_targets.csv")
-    print("Getting ids of assays with nsp13")
+    nsp13_sequence_path = pathlib.Path("./nsp13.fasta")
+    sequence_subset_path = pathlib.Path(
+        "./activities_targets/sequence_subset_assay_id.fa"
+    )
+
     ids = get_nsp13_chembl_assay_ids(assays_metadata_path, "nsp13|sars-.*-2")
 
-    with open("./nsp13.fasta", "r") as file:
+    with open(nsp13_sequence_path, "r") as file:
         nsp13_sequence_lines = file.readlines()[1:]
-       
-    with open("./activities_targets/sequence_subset_assay_id.fa", "a") as output:
+
+    with open(sequence_subset_path, "r") as output:
+        ids_in_file = set(
+            line.strip(">\n") for line in output.readlines() if line.startswith(">")
+        )
+        if ids_in_file.intersection(set(ids)):
+            print(
+                "[\033[33mwarning\033[0m] Some of assay ids that would be "  # ]]
+                "appended to sequence subset file are already there."
+            )
+            choice = input("Do you wish to append nsp13 with those ids anyway: [Y/n]: ")
+            if choice.lower() not in ["y", "yes"]:
+                return 1
+
+    with open(sequence_subset_path, "a") as output:
         for assay_id in ids:
             lines = [f">{assay_id}\n"] + nsp13_sequence_lines
             output.writelines(lines)
